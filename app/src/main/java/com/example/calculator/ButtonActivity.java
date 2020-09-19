e97673254ed3f0dc15aafc71c690a35edcf3a0a4
Andrei Makarenko <andrei.makarenko.1998@gmail.com>
2020-09-19 17:11:08 +0300

    Add preparing result

--- app/src/main/java/com/example/calculator/ButtonActivity.java
+++ app/src/main/java/com/example/calculator/ButtonActivity.java
@@ -1,5 +1,9 @@
 package com.example.calculator;
 
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +14,7 @@ import org.w3c.dom.Text;
 
 public class ButtonActivity {
     private TextView inputField;
+    private TextView resultField;
     private MainActivity layout;
     private final int[] inputElements = new int[] {
             R.id.button_1, R.id.button_2, R.id.button_3, R.id.button_4, R.id.button_5, R.id.button_6, R.id.button_7, R.id.button_8,
@@ -18,6 +23,8 @@ public class ButtonActivity {
     public ButtonActivity(MainActivity layout) {
         this.layout = layout;
         inputField = layout.findViewById(R.id.input_field);
+        resultField = layout.findViewById(R.id.result_field);
+
     }
 
     public void addEventsToButtons() {
@@ -29,9 +36,26 @@ public class ButtonActivity {
                 }
             });
         }
+
+        layout.findViewById(R.id.equal).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                calculateResult((String) inputField.getText());
+            }
+        });
     }
 
     private void addValueToView(String newValue) {
           inputField.setText(inputField.getText() + newValue);
+    }
+
+    private void calculateResult(String expressionString) {
+        try {
+            ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+            String result = String.valueOf(engine.eval(expressionString));
+
+            resultField.setText(result);
+        } catch (ScriptException e) {
+            System.err.println("Error evaluating the script: " + e.getMessage());
+        }
     }
 }
